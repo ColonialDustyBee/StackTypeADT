62bb5e4b6b69c9aaaec311109efd597f7f565a4d
Jesus <111938653+ColonialDustyBee@users.noreply.github.com>
2022-09-27 15:06:26 -0700

    Updated Pop and Removed Quit Function
    Pop now returns a value and Quit is now just implemented into the driver file.

--- stackADTHeader.h
+++ stackADTHeader.h
@@ -6,13 +6,10 @@ class Stack {
 private:
 	int top = 0; // Keeps track of length
 	T* items = new T[top]; // Allocate memory of T based items 
-	
 public:
-	Stack();
-	void Pop(); // Simply removes first item on the list. 
+	Stack(); // Constructor;
+	T Pop(); // Simply removes first item on the list. 
 	void Push(T); // Gets item and pushes it into the list. 
 	bool isEmpty() const;
-	bool Quit() const; // meant to quit out of adt
-	T GetRecentElement() const;
 };
 #endif
